07747ebbd72e55b6cbeea11c2455e8af33c8797d
bfrancesco <brunofrancescobarbara@gmail.com>
2021-10-28 09:02:33 +0200

    Primo commmit creazione progetto

---
+++ src/main/java/unical/it/ProvaIngSw_209886/App.java
@@ -0,0 +1,13 @@
+package unical.it.ProvaIngSw_209886;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
